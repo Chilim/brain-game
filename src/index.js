49be4e0af2205e4a0dd7a5d5ad51b7cf3f41fe8d
Davud Kakhrimanov <davud.kakhriman@gmail.com>
2017-09-22 02:10:19 +0300

    the game engine is fixed

--- src/index.js
+++ src/index.js
@@ -1,30 +1,31 @@
 import readlineSync from 'readline-sync';
 
-const greetings = 'Welcome to the Brain Games!';
 const succeed = 'Congratulations';
-const iterNum = 0;
+const numOfCorrect = 3; // мне кажется, что снизу вверх логичнее - кол-во правильных ответов 1, 2, 3
 
 const getResult = (userName, getGameContent) => {
   const iter = (acc) => {
-    if (acc === 3) {
+    if (acc === 0) {
       console.log(`${succeed}, ${userName}!`);
       return 'End Game';
     }
     const gameContent = getGameContent();
-    const userAnswer = readlineSync.question(`Enter result: ${gameContent.question} `);
+    const userAnswer = readlineSync.question(`Question: ${gameContent.question} `);
     console.log(`Your answer: ${userAnswer}`);
     if (userAnswer === gameContent.correctAnswer) {
       console.log('Correct!');
-      return iter(acc + 1);
+      return iter(acc - 1);
     }
-    console.log(`${userAnswer} is wrong answer. Correct answer was ${gameContent.correctAnswer}`);
-    return iter(acc);
+    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${gameContent.correctAnswer}'`);
+    console.log(`Let's try again, ${userName}!`);
+    return 'The end';
   };
-  return iter(iterNum);
+  return iter(numOfCorrect);
 };
 
 export default (condition, getGameContent) => {
-  console.log(`${greetings}\n ${condition}`);
+  console.log('Welcome to the Brain Games!');
+  console.log(`${condition}`);
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   return getResult(userName, getGameContent);
