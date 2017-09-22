a257251610c2679ca52742796183120b7236283a
Davud Kakhrimanov <davud.kakhriman@gmail.com>
2017-09-22 03:20:07 +0300

    fixed

--- dist/bin/brain-calc.js
+++ dist/bin/brain-calc.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 'use strict';
 
 var _brainCalc = require('../games/brain-calc');
@@ -7,4 +8,4 @@ var _brainCalc2 = _interopRequireDefault(_brainCalc);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 (0, _brainCalc2.default)();
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9iaW4vYnJhaW4tY2FsYy5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOztBQUFBOzs7Ozs7QUFFQSIsImZpbGUiOiJicmFpbi1jYWxjLmpzIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IGJyYWluQ2FsYyBmcm9tICcuLi9nYW1lcy9icmFpbi1jYWxjJztcblxuYnJhaW5DYWxjKCk7XG4iXX0=
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9iaW4vYnJhaW4tY2FsYy5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOztBQUVBOzs7Ozs7QUFFQSIsImZpbGUiOiJicmFpbi1jYWxjLmpzIiwic291cmNlc0NvbnRlbnQiOlsiXG5cbmltcG9ydCBicmFpbkNhbGMgZnJvbSAnLi4vZ2FtZXMvYnJhaW4tY2FsYyc7XG5cbmJyYWluQ2FsYygpO1xuIl19
