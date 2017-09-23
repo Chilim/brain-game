dcd387181db41f25e31f7ce73db8b70a0d53aeb6
Davud Kakhrimanov <davud.kakhriman@gmail.com>
2017-09-23 19:21:17 +0300

    fixed

--- src/games/prime.js
+++ src/games/prime.js
@@ -3,14 +3,14 @@ import getRandNum from '../utils';
 
 const task = 'Answer "yes" if number is prime otherwise answer "no".';
 
-const isPrime = (a) => {
-  const iter = (number) => {
-    if (Math.sqrt(a) < number) {
+const isPrime = (randNum) => {
+  const iter = (divider) => {
+    if (Math.sqrt(randNum) < divider) {
       return true;
-    } else if (a % number === 0) {
+    } else if (randNum % divider === 0) {
       return false;
     }
-    return iter(number + 1);
+    return iter(divider + 1);
   };
   return iter(2);
 };
